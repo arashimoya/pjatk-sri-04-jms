8356764c794175ac292736aed3a4604be517f078
s34092 <arasimovicadam@gmail.com>
2025-04-26 19:25:37 +0200

    init

---
+++ src/main/java/edu/pja/sri/s34092/sri04/Sri04Application.java
@@ -0,0 +1,13 @@
+package edu.pja.sri.s34092.sri04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sri04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sri04Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/pja/sri/s34092/sri04/Sri04ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pja.sri.s34092.sri04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sri04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
